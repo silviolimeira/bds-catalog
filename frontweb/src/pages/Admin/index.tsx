39cf287aece54eef19971d8edd4a57f9ddce8e1f
Silvio Limeira <sicemal@gmail.com>
2023-03-17 21:07:20 -0400

    initial commit

---
+++ frontweb/src/pages/Admin/index.tsx
@@ -0,0 +1,29 @@
+import { Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Users from './User';
+import PrivateRoute from 'components/PrivateRoute';
+
+import './styles.css';
+
+const Admin = () => {
+  return (
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+        <Switch>
+          <PrivateRoute path="/admin/products">
+            <h1>Product CRUD</h1>
+          </PrivateRoute>
+          <PrivateRoute path="/admin/categories">
+            <h1>Category CRUD</h1>
+          </PrivateRoute>
+          <PrivateRoute path="/admin/users" roles={['ROLE_ADMIN']}>
+            <Users />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
