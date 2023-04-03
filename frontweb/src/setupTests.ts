7a3dea31b363adb00fcec1ce9ceb9cbf06c026e0
Silvio Limeira <sicemal@gmail.com>
2023-04-03 09:21:43 -0400

    First test

---
+++ frontweb/src/setupTests.ts
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+
+// yarn add @testing-library/jest-dom@5.14.1 @testing-library-react@12.0.0 @testing-library-user-event@13.2.1
+
+
+
