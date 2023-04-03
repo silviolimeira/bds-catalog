441478fbf91f1831b03404be9c9ae131a48eda74
Silvio Limeira <sicemal@gmail.com>
2023-04-03 10:09:42 -0400

    REFACTOR token, Rone

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,13 +1,38 @@
 import { formatPrice } from "util/formatters";
 
-test('formatPrice should format number pt-BR when given 10.1', () => {
+describe('formatPrice for positive numbers', () => {
 
-    // ARRANGE
-    const value = 10.1;
+    test('formatPrice should format number pt-BR when given 10.1', () => {
+        // ARRANGE
+        const value = 10.1;
+        // ACT
+        const result = formatPrice(value);
+        // ASSERT
+        expect(result).toEqual("10,10");
+    })
 
-    // ACT
-    const result = formatPrice(value);
+    test('formatPrice should format number pt-BR when given 0.1', () => {
+        const value = 0.1;
+        const result = formatPrice(value);
+        expect(result).toEqual("0,10");
+    })
 
-    // ASSERT
-    expect(result).toEqual("10,10");
+})
+
+
+describe('formatPrice for non-positive numbers', () => {
+
+    test('formatPrice should format number pt-BR when given 0.0', () => {
+        const value = 0.0;
+        const result = formatPrice(value);
+        expect(result).toEqual("0,00");
+    })
+
+    test('formatPrice should format number pt-BR when given -5.1', () => {
+        const value = -5.1;
+        const result = formatPrice(value);
+        expect(result).toEqual("-5,10");
+    })
+
+
 })
